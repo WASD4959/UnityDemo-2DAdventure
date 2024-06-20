a5801fe48b60276fbeac3b41ba997eed9f354a13
Putuitou <wasd_4959@163.com>
2024-06-20 15:24:09 +0800

    Pause Panel & Volume Control

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -9,19 +9,41 @@ public class AudioManager : MonoBehaviour
     [Header("事件监听")]
     public PlayAudioEventSO FXEvent;
     public PlayAudioEventSO BGMEvent;
+    public FloatEventSO volumeEvent;
+    public VoidEventSO pauseEvent;
+
+    [Header("广播")]
+    public FloatEventSO syncVolumeEvent;
 
     [Header("组件")]
     public AudioSource BGMSource;
     public AudioSource FXSource;
+    public AudioMixer mixer;
 
     private void OnEnable() {
         FXEvent.onEventRaised += OnFXEvent;
         BGMEvent.onEventRaised += onBGMEvent;
+        volumeEvent.OnEventRaised += OnVolumeEvent;
+        pauseEvent.OnEventRaised += OnPauseEvent;
     }
 
     private void OnDisable() {
         FXEvent.onEventRaised -= OnFXEvent;
         BGMEvent.onEventRaised -= onBGMEvent;
+        volumeEvent.OnEventRaised -= OnVolumeEvent;
+        pauseEvent.OnEventRaised -= OnPauseEvent;
+    }
+
+    private void OnPauseEvent()
+    {
+        float amount;
+        mixer.GetFloat("MasterVolume", out amount);
+        syncVolumeEvent.RaiseEvent(amount);
+    }
+
+    private void OnVolumeEvent(float amount)
+    {
+        mixer.SetFloat("MasterVolume", amount * 100 - 80);
     }
 
     private void onBGMEvent(AudioClip audioClip)
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -14,18 +15,26 @@ public class UIManager : MonoBehaviour
     public VoidEventSO loadDataEvent;
     public VoidEventSO gameOverEvent;
     public VoidEventSO backToMenuEvent;
+    public FloatEventSO syncVolumeEvent;
+
+    [Header("广播")]
+    public VoidEventSO PauseEvent;
 
     [Header("组件")]
     public GameObject gameOverPanel;
     public GameObject restartBtn;
     public GameObject mobileTouch;
+    public Button settingBtn;
+    public GameObject pausePanel;
+    public Slider volumeSlider;
 
     private void Awake() {
         #if UNITY_STANDALONE
             mobileTouch.SetActive(false);
         #endif
-    }
 
+        settingBtn.onClick.AddListener(TogglePausePanel);
+    }
 
     private void OnEnable() {
         healthEvent.OnEventRaised += OnHealthEvent;
@@ -33,6 +42,7 @@ public class UIManager : MonoBehaviour
         loadDataEvent.OnEventRaised += OnLoadDataEvent;
         gameOverEvent.OnEventRaised += OnGameOverEvent;
         backToMenuEvent.OnEventRaised += OnLoadDataEvent;
+        syncVolumeEvent.OnEventRaised += OnSyncVolumeEvent;
     }
 
     private void OnDisable() {
@@ -41,6 +51,12 @@ public class UIManager : MonoBehaviour
         loadDataEvent.OnEventRaised -= OnLoadDataEvent;
         gameOverEvent.OnEventRaised -= OnGameOverEvent;
         backToMenuEvent.OnEventRaised -= OnLoadDataEvent;
+        syncVolumeEvent.OnEventRaised -= OnSyncVolumeEvent;
+    }
+
+    private void OnSyncVolumeEvent(float amount)
+    {
+        volumeSlider.value = (amount + 80)/100;
     }
 
     private void OnLoadDataEvent()
@@ -67,4 +83,12 @@ public class UIManager : MonoBehaviour
         playerStatBar.OnHealthChange(percentage);
         playerStatBar.OnPowerChange(character);
     }
+
+    private void TogglePausePanel()
+    {
+        if(Time.timeScale == 1) PauseEvent.RaiseEvent();
+        pausePanel.SetActive(!pausePanel.activeInHierarchy);
+        Time.timeScale = 1 - Time.timeScale;
+    }
 }
+ 
