894e23b5c2fa4f1fe046b3c6693d0f991307fee3
Putuitou <wasd_4959@163.com>
2024-06-21 14:38:35 +0800

    Pack Game

--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using Unity.VisualScripting;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
--- Assets/Scripts/Player/Sign.cs
+++ Assets/Scripts/Player/Sign.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XInput;
