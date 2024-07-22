39d6a08316d4b59a6ad72800d54a9ebcd0494e99
Putuitou <wasd_4959@163.com>
2024-07-22 14:27:11 +0800

    Winning Panel UI and Audio update

--- Assets/Scripts/General/Trophy.cs
+++ Assets/Scripts/General/Trophy.cs
@@ -7,9 +7,24 @@ public class Trophy : MonoBehaviour
     [Header("广播")]
     public VoidEventSO gameWinEvent;
 
+    public float amplitude = 0.5f;
+    public float frequency = 0.5f;
+
+    private Vector3 startPos;
+
+    private void Start() {
+        startPos = transform.position;
+    }
+
+    private void Update() {
+        float newY = startPos.y + amplitude * Mathf.Sin(Time.time * frequency);
+        transform.position = new Vector3(startPos.x, newY, startPos.z);
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")){
             gameWinEvent.RaiseEvent();
+            GetComponent<AudioDefination>()?.PlayAudioClip(); 
         }
     }
 }
